a2a2d714a9994bcb61d15a2cc53550154ba79af9
unknown <ozhan728@gmail.com>
2024-03-13 22:47:18 +0330

    validate duplicate username and email

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.Form):
 
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'your username','class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'email@site.com','class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'your password'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user :
+            raise ValidationError('this email already exists')
+
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user :
+            raise ValidationError('this username already exists')
+
+        return username
+
+
