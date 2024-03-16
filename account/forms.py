3e7c6c14d2570d64d1cbb38900e000a64f71c9be
unknown <ozhan728@gmail.com>
2024-03-16 21:11:01 +0330

    password check

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,8 @@ class UserRegistrationForm(forms.Form):
 
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'your username','class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'email@site.com','class':'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'your password'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'your password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'your password'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -23,5 +24,13 @@ class UserRegistrationForm(forms.Form):
             raise ValidationError('this username already exists')
 
         return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2 :
+            raise ValidationError('Entered passwords does not match')
 
 
--- account/views.py
+++ account/views.py
@@ -19,7 +19,7 @@ class RegisterView(View) :
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'],cd['email'],cd['password'])
+            User.objects.create_user(cd['username'],cd['email'],cd['password1'])
             messages.success(request,'User created successfully','success')
             return redirect('home:home')
 
