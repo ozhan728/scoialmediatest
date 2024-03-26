b1689c4a34167c4b74f47879d1e55b366bc6b7c3
unknown <ozhan728@gmail.com>
2024-03-26 23:11:44 +0330

    fixed logout bug

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from .forms import UserRegistrationForm , UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -61,7 +62,7 @@ class UserLoginView(View):
         return render(request,self.templates_name,{'form':self.form_class})
 
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin,View):
     def get(self,request):
         logout(request)
         messages.success(request,'you logged out successfully','success')
