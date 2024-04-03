dfa36d2cc7d697d497227d06b45d9fbdb4fc66be
unknown <ozhan728@gmail.com>
2024-04-03 19:01:29 +0330

    added update

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
 # Create your views here.
 
 # def home(request) :
@@ -31,3 +32,20 @@ class PostDeleteView( LoginRequiredMixin ,View):
             messages.error(request,'you cant delete this post ','danger')
 
         return redirect('home:home')
+
+class PostUpdateView(LoginRequiredMixin,View):
+    form_class = PostUpdateForm
+    def dispatch(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        if not post.user.id == request.user.id :
+            messages.error(request,'you cant access this post ','danger')
+            return redirect('home:home')
+        return super().dispatch(request,*args,**kwargs)
+
+    def get(self,request,post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return render(request,'home/update.html',{'form': form})
+
+    def post(self,request,post_id):
+        pass
