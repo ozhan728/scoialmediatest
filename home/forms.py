529d148e86176ce3c2600b72bf204085bc41ba60
unknown <ozhan728@gmail.com>
2024-04-16 19:32:08 +0330

    reply implemented alpha

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Post
-
+from .models import Post , Comment
 class PostUpdateForm(forms.ModelForm):
     class Meta :
         model = Post
         fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'class':'form-control','rows':8})
+        }
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta :
+        model = Comment
+        fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'class':'form-control','rows':4})
+        }
